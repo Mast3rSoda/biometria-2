df183872fe65a07cfb2405cf3fafd4aaa94a365b
Mast3rSoda <63344754+Mast3rSoda@users.noreply.github.com>
2022-03-15 22:24:30 +0100

    Initial commit

---
+++ biometria-2/Algorithm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biometria_2;
+
+public static class Algorithm
+{
+    public static Bitmap Histogram(Bitmap bmp, int[] histogram)
+    {
+        var data = bmp.LockBits(
+            new Rectangle(0, 0, bmp.Width, bmp.Height),
+            System.Drawing.Imaging.ImageLockMode.ReadWrite,
+            System.Drawing.Imaging.PixelFormat.Format24bppRgb
+        );
+        var bmpData = new byte[data.Stride * data.Height];
+        for (int i = 0; i < bmpData.Length; i++)
+            bmpData[i] = 255;
+        for (int i = 0; i < histogram.Length; i++)
+        {
+            for (int j = 0; j < histogram[i]; j++)
+            {
+                int index = i * 3 + (data.Height - 1 - j) * data.Stride;
+
+                bmpData[index + 0] =
+                bmpData[index + 1] =
+                bmpData[index + 2] = 0;
+            }
+        }
+
+        Marshal.Copy(bmpData, 0, data.Scan0, bmpData.Length);
+        // Przerzuci z tablicy do Bitmapy
+
+        bmp.UnlockBits(data);
+        return bmp;
+    }
+
+    public static int[] getHistogramData(Bitmap bmp)
+    {
+        var data = bmp.LockBits(
+            new Rectangle(0, 0, bmp.Width, bmp.Height),
+            System.Drawing.Imaging.ImageLockMode.ReadWrite,
+            System.Drawing.Imaging.PixelFormat.Format24bppRgb
+        );
+        var bmpData = new byte[data.Stride * data.Height];
+
+        Marshal.Copy(data.Scan0, bmpData, 0, bmpData.Length);
+        // Przerzuci z Bitmapy do tablicy
+
+        int[] histogram = new int[256];
+        foreach (byte i in bmpData)
+            ++histogram[i];
+        double max = histogram.Max();
+        for (int i = 0; i < histogram.Length; i++)
+            histogram[i] = (int)(histogram[i] / max * (double)data.Height);
+
+        return histogram;
+    }
+}
---
+++ biometria-2/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace biometria_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Bitmap? sourceImage = null;
+        int[]? histogramValues = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFile(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.jpg;*.png)|*.jpg;*.png|All files (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string fileName = openFileDialog.FileName;
+                this.sourceImage = new Bitmap($"{fileName}");
+                OriginalImage.Source = ImageSourceFromBitmap(this.sourceImage);
+                histogramValues = Algorithm.getHistogramData(new Bitmap($"{fileName}"));
+                HistogramImage.Source = ImageSourceFromBitmap(Algorithm.Histogram(new Bitmap($"{fileName}"), histogramValues));
+            }
+        }
+
+        [DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool DeleteObject([In] IntPtr hObject);
+        public ImageSource ImageSourceFromBitmap(Bitmap bmp)
+        {
+            var handle = bmp.GetHbitmap();
+            try
+            {
+                return Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            }
+            finally { DeleteObject(handle); }
+        }
+
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+}
